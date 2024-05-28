04434c288eefe6027ceb98cfdfff0c9ceec3acac
wm4jcllmnbgwl <dc04a8ed2a7648c0a6278133a544f837@example.com>
2024-05-29 01:08:04 +0800

    更新 views.py

--- SSM_0/school/views.py
+++ SSM_0/school/views.py
@@ -102,6 +102,8 @@ class UserProfileViewSet(viewsets.ViewSet):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data)
+        else:
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class StudentCreateView(CreateModelMixin, GenericAPIView):
